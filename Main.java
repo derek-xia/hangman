ab4a95f98b2ec4d72f0cb038d93fb2fa0bdab007
derek-xia <derek.xia@gmail.com>
2016-10-31 16:17:32 -0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -5,22 +6,20 @@ public class Main {
     public static void main(String[] args) {
 	    Scanner sc = new Scanner(System.in);
         System.out.println("To play single-player, enter '1'. To play with 2 players, enter '2'.");
-        if(sc.hasNextInt()) {
+        try {
             int players = sc.nextInt();
-            if(players == 1){
+            if (players == 1) {
                 Singleplayer game = new Singleplayer();
                 game.start();
-            }
-            else if(players == 2){
+            } else if (players == 2) {
                 Multiplayer game = new Multiplayer();
                 game.start();
-            }
-            else{
+            } else {
                 System.out.println(players + " is not a valid number.");
             }
 
         }
-        else{
+        catch(InputMismatchException e){
             System.out.println("That is not a valid number.");
         }
 
--- Singleplayer.java
+++ Singleplayer.java
@@ -1,15 +1,19 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.*;
+import java.io.FileReader;
 
 class Singleplayer {
     static Scanner sc = new Scanner(System.in);
-    private int lives = 10;
-    static String[] words = {"pie","sky","heads"};
+    private int maxlives = 10;
 
     public void start(){
+
         System.out.println("Welcome to Hangman. Please enter a guess:");
         Random rng = new Random();
+        String[] words = checkWords();
         String word = words[rng.nextInt(words.length)];
-        Integer win = this.guess(word);
+        Integer win = this.guess(word,maxlives);
 
         switch (win) {
             case 1:
@@ -20,42 +24,116 @@ class Singleplayer {
                 break;
         }
     }
-    public final Integer guess(String w){
-        int lettersGuessed = 0;
-        char[] SelectedWord= w.toCharArray();
-        char[] InputtedWord;
-        int WinLose = 2;
-        InputtedWord = new char[w.length()];
-        List<String> WrongGuess = new ArrayList<String>();
 
-        System.out.print("Lives: " + lives + "    ");
-        for (int a = 0; a<w.length(); a++){
-            InputtedWord[a] = '_';
-            System.out.print("_ ");
+    public String[] checkWords(){
+
+        try{
+            FileReader filereader = new FileReader(new File("WordList.txt"));
+            Scanner fileInput = new Scanner(filereader);
+
+            ArrayList<String> wd = new ArrayList<String>();
+            fileInput.nextLine(); //skips the first line of text
+
+            while(fileInput.hasNextLine()){
+                wd.add(fileInput.nextLine());
+            }
+            String[] inputwordlist = new String[wd.size()];
+            wd.toArray(inputwordlist);
+            return inputwordlist;
         }
-        System.out.println();
+
+        catch(FileNotFoundException e){
+            System.out.println("Word file not found.");
+            String[] inputwordlist = {"pie","sky","heads"};
+            return inputwordlist;
+        }
+
+        catch(IllegalArgumentException e){
+            System.out.println("Add words to the list.");
+            String[] inputwordlist = {"pie","sky","heads"};
+            return inputwordlist;
+        }
+
+    }
+
+    public final Integer guess(String w,int lives){
+        int WinLose = 2;
+        //default lose
+        lives = maxlives;
+
+        char[] SelectedWord = w.toCharArray(); //
+        char[] InputtedWord; //string to print out
+        InputtedWord = new char[w.length()];
+
+        int charLocation = 0; //
+        for(char c:w.toCharArray()){
+            if(c == ' ' ){
+                InputtedWord[charLocation] = ' ';
+                System.out.print(" ");
+            }
+            if(c == '-' ){
+                InputtedWord[charLocation] = '-';
+                System.out.print("- ");
+            }
+            if(c!=' ' && c!='-'){
+                InputtedWord[charLocation] = '_';
+                System.out.print("_ ");
+            } //makes inputted word to underscore and -/space and prints it out
+
+        charLocation++;
+        }
+
+        List<Character> AlreadyGuessed = new ArrayList<Character>();
+
+        System.out.print("Lives: ");
+        for(int h = 0;h<lives;h++){
+            System.out.print("♥");
+        }
+        System.out.println("   \n ");
         while(lives>0){
 
             char input = sc.next().charAt(0);
             Boolean guessed = false;
-            int curr = 0;
+            Boolean repeat = false;
+            int curr = 0; //iterator to check through selectedword
+            for(int it=0;it<AlreadyGuessed.size();it++){//check for already typed character
+                if(input == AlreadyGuessed.get(it)){
+                    System.out.println("This letter has already been guessed. Please try another letter.");
+                    repeat = true;
+                    guessed = true;
+                    break;
+                }
+            }
+
             for(char C : SelectedWord){
-                if (input == SelectedWord[curr]){
+                if (input == SelectedWord[curr] && repeat == false){ //if entered character is equal to any existing value
                     InputtedWord[curr] = input;
                     guessed = true;
                 }
                 curr++;
             }
+
             if (!guessed){
                 lives--;
-                WrongGuess.add(String.valueOf(input));
             }
 
-            System.out.print("Lives: " + lives + "    ");
-            for (char C: InputtedWord) {
-                System.out.print(C+" ");
+            if(repeat==false){
+                AlreadyGuessed.add(input);
+                System.out.print("Lives: ");
+                for(int h = 0;h<lives;h++){
+                    System.out.print("♥");
+                }
+                /*for(int h = 0;h<maxlives-lives;h++){
+                    System.out.print(" ♡");
+                }*/
+                System.out.println("     ");
+                for (char C: InputtedWord) {
+                    System.out.print(C+" ");
+                }
+                System.out.println();
             }
-            System.out.println();
+
+            System.out.println("Previously guessed letters:"+AlreadyGuessed+"\n");
 
             if (Arrays.equals(InputtedWord,SelectedWord)){
                 WinLose = 1;
